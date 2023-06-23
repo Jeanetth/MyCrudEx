098a3b9438ffbc54d4099170abab123fd77f90e0
Moises Timoteo Perez Reyes <pr20028@ues.edu.sv>
2023-06-23 15:56:05 -0600

    Se crea el proyecto

---
+++ app/src/main/java/com/ues/edu/sv/ipam/crudipam/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ues.edu.sv.ipam.crudipam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
