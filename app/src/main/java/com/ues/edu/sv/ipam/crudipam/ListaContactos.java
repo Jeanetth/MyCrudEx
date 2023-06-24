b68d5d7eab8f01d44416882c6c7c2565670b0372
Moises Timoteo Perez Reyes <pr20028@ues.edu.sv>
2023-06-24 16:31:03 -0600

    Se agrega boton de eliminar

--- app/src/main/java/com/ues/edu/sv/ipam/crudipam/ListaContactos.java
+++ app/src/main/java/com/ues/edu/sv/ipam/crudipam/ListaContactos.java
@@ -1,6 +1,9 @@
 package com.ues.edu.sv.ipam.crudipam;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -13,22 +16,32 @@ import java.util.List;
 
 public class ListaContactos extends AppCompatActivity {
 
-
+    Button buttonReturn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_contactos);
-
+        buttonReturn = findViewById(R.id.btn_return2);
+        buttonReturn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ListaContactos.this, MainActivity.class );
+                startActivity(intent);
+            }
+        });
         List<Contactos> contactosList = new ArrayList<>();
-        Contactos contacto1 = new Contactos(1, "Nombre 1", "123456789");
-        Contactos contacto2 = new Contactos(2, "Nombre 2", "987654321");
-        Contactos contacto3 = new Contactos(3, "Nombre 3", "456789123");
-        Contactos contacto4 = new Contactos(4, "Nombre 4", "456789123");
+        Contactos contacto1 = new Contactos(1, "Juan", "123456789");
+        Contactos contacto2 = new Contactos(2, "Ricardo", "987654321");
+        Contactos contacto3 = new Contactos(3, "Godolfredo", "456789123");
+        Contactos contacto4 = new Contactos(4, "Enrique", "456789123");
+        Contactos contacto5 = new Contactos(4, "Enrique", "456789123");
+
 
         contactosList.add(contacto1);
         contactosList.add(contacto2);
         contactosList.add(contacto3);
         contactosList.add(contacto4);
+        contactosList.add(contacto5);
 
         RecyclerView recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(this)); // Puedes usar cualquier otro LayoutManager según tus necesidades
