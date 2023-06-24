c8fd2916066a8b6ab47c05ec84e8ce99d7ac5fb2
Moises Timoteo Perez Reyes <pr200028@ues.edu.sv>
2023-06-24 09:51:32 -0600

    Se agrega intent para redirrecionar

--- app/src/main/java/com/ues/edu/sv/ipam/crudipam/AgregarContactos.java
+++ app/src/main/java/com/ues/edu/sv/ipam/crudipam/AgregarContactos.java
@@ -2,13 +2,26 @@ package com.ues.edu.sv.ipam.crudipam;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AgregarContactos extends AppCompatActivity {
 
+    Button buttonReturn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agregar_contactos);
+
+        buttonReturn = findViewById(R.id.btn_return);
+        buttonReturn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AgregarContactos.this, MainActivity.class );
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/ues/edu/sv/ipam/crudipam/MainActivity.java
+++ app/src/main/java/com/ues/edu/sv/ipam/crudipam/MainActivity.java
@@ -2,13 +2,28 @@ package com.ues.edu.sv.ipam.crudipam;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button buttonAdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonAdd = findViewById(R.id.btn_add);
+
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AgregarContactos.class);
+                startActivity(intent);
+            }
+        });
     }
 }
