ec6c7427fca03fa589c0673233de499aaad79897
Carlos Zepeda <zv20006@ues.edu.sv>
2023-06-23 20:42:32 -0600

    Diseño de activity Agregar contactos

---
+++ app/src/main/java/com/ues/edu/sv/ipam/crudipam/AgregarContactos.java
@@ -0,0 +1,14 @@
+package com.ues.edu.sv.ipam.crudipam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AgregarContactos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agregar_contactos);
+    }
+}
