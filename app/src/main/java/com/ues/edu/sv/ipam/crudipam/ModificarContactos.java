3acd7950a3367756cff9577c1edd6420aa616549
Mariana Jeaneth Gochez Santos <GS20027@UES.EDU.SV>
2023-06-25 23:38:52 -0600

    Boton actualizar finalizado

--- app/src/main/java/com/ues/edu/sv/ipam/crudipam/ModificarContactos.java
+++ app/src/main/java/com/ues/edu/sv/ipam/crudipam/ModificarContactos.java
@@ -8,10 +8,19 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.ues.edu.sv.ipam.crudipam.sqlLite.CapaBaseDatos;
 import com.ues.edu.sv.ipam.crudipam.sqlLite.Contactos;
 
 public class ModificarContactos extends AppCompatActivity {
+    String nombre;
+    String telefono;
+    String apellido;
+    String edad;
+    String domicilio;
+    String correo;
     Button buttonReturn;
+    Button buttonActualizar;
 
     EditText editNombre;
     EditText editTelefono;
@@ -46,6 +55,35 @@ public class ModificarContactos extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(ModificarContactos.this, ListaContactos.class );
                 startActivity(intent);
+            }
+        });
+        buttonActualizar = findViewById(R.id.actualizar);
+        buttonActualizar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                nombre = editNombre.getText().toString();
+                telefono = editTelefono.getText().toString();
+                apellido = editApellido.getText().toString();
+                domicilio = editDomicilio.getText().toString();
+                edad= editEdad.getText().toString();
+                correo=editCorreo.getText().toString();
+
+                Contactos contacto = new Contactos();
+                CapaBaseDatos datos = new CapaBaseDatos(getApplicationContext());
+
+                contacto.setNombre(nombre);
+                contacto.setTelefono(telefono);
+                contacto.setCorreo(correo);
+                contacto.setEdad(edad);
+                contacto.setDomicilio(domicilio);
+                contacto.setApellido(apellido);
+                contacto.setID(contactoSeleccionado.getID());
+                datos.updateContacto(contacto);
+                Toast.makeText(getApplicationContext(),"contacto agregado", Toast.LENGTH_SHORT).show();
+
+                Intent intent = new Intent(ModificarContactos.this, ListaContactos.class );
+                startActivity(intent);
+
             }
         });
     }
